e1ed8e9dc789b4471dea527a5cd2ece8f23e0358
KulaniMelaku65 <40515098+KulaniMelaku65@users.noreply.github.com>
2023-11-15 11:16:28 +0300

    Update Notes using gui

--- Notes App.py
+++ Notes App.py
@@ -1,29 +1,82 @@
-notes = []
+import os
 
-def add_note(text):
-   notes.append(text)
-   print('Note added')
+def show_menu():
+    print("Simple Notes App")
+    print("1. View Notes")
+    print("2. Add Note")
+    print("3. Edit Note")
+    print("4. Delete Note")
+    print("5. Exit")
 
-def display_notes():
-   for note in notes:
-      print(note)
+def view_notes():
+    try:
+        with open("notes.txt", "r") as file:
+            notes = file.readlines()
+            if notes:
+                for idx, note in enumerate(notes, start=1):
+                    print(f"{idx}. {note.strip()}")
+            else:
+                print("No notes found.")
+    except FileNotFoundError:
+        print("No notes found.")
 
-while True:
-   print("1. Add Note") 
-   print("2. View Notes")
-   print("3. Quit")
+def add_note():
+    note = input("Enter your note: ")
+    with open("notes.txt", "a") as file:
+        file.write(note + "\n")
+    print("Note added successfully.")
 
-   choice = input("Enter choice: ")
+def edit_note():
+    view_notes()
+    try:
+        note_number = int(input("Enter the note number you want to edit: "))
+        with open("notes.txt", "r") as file:
+            notes = file.readlines()
+        if 1 <= note_number <= len(notes):
+            new_note = input("Enter the new note: ")
+            notes[note_number - 1] = new_note + "\n"
+            with open("notes.txt", "w") as file:
+                file.writelines(notes)
+            print("Note edited successfully.")
+        else:
+            print("Invalid note number.")
+    except ValueError:
+        print("Invalid input. Please enter a valid note number.")
 
-   if choice == '1':
-      note = input("Enter note: ")
-      add_note(note)
-      
-   elif choice == '2':   
-      display_notes()
-      
-   elif choice == '3':
-      break
-      
-   else:
-      print("Invalid choice")
+def delete_note():
+    view_notes()
+    try:
+        note_number = int(input("Enter the note number you want to delete: "))
+        with open("notes.txt", "r") as file:
+            notes = file.readlines()
+        if 1 <= note_number <= len(notes):
+            del notes[note_number - 1]
+            with open("notes.txt", "w") as file:
+                file.writelines(notes)
+            print("Note deleted successfully.")
+        else:
+            print("Invalid note number.")
+    except ValueError:
+        print("Invalid input. Please enter a valid note number.")
+
+def main():
+    while True:
+        show_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            view_notes()
+        elif choice == "2":
+            add_note()
+        elif choice == "3":
+            edit_note()
+        elif choice == "4":
+            delete_note()
+        elif choice == "5":
+            print("Exiting the notes app. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please choose a valid option.")
+
+if __name__ == "__main__":
+    main()
